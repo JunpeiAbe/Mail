59ef42f275043527fade8adbfbc80d78188abd95
JunpeiAbe <uihrsw0228@gmail.com>
2025-01-02 17:55:39 +0900

    WKWebViewのシーケンス図とログ追加

--- Mail/View/Web/WebViewController.swift
+++ Mail/View/Web/WebViewController.swift
@@ -52,27 +52,29 @@ final class WebViewController: UIViewController {
 extension WebViewController: WKNavigationDelegate {
     /// ページ読み込み完了時😄
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        
+        print("ページ読み込み完了")
     }
     /// ナビゲーションリクエストの許可または拒否(タイミング：リクエスト前)🤔
     /// - note: 指定された設定やアクション（タップなど）に基づいて、新しいコンテンツに移動する許可または拒否を応答
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        print("リクエスト前")
         decisionHandler(.allow)
     }
     /// ナビゲーションリクエストの許可または拒否(タイミング：レスポンス後)🤔
     /// - note:  webViewが元のURLリクエストに対する応答を受信した後、ナビゲーションリクエストを許可または拒否するには、このメソッドを使用
     func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse) async -> WKNavigationResponsePolicy {
+        print("レスポンス後")
         return .allow
     }
     /// リクエストのロード進行状況の追跡(タイミング：ページ読み込み開始前)🔍
     /// - note: ナビゲーションリクエストを処理するための暫定的な承認を受け取った後、そのリクエストに対するレスポンスを受け取る前にこのメソッドを呼び出し
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        print("didStartProvisionalNavigation navigation: WKNavigation!")
+        print("ページ読み込み開始前")
     }
     /// リクエストのロード進行状況の追跡(タイミング：ページ読み込み開始時)🔍
     /// - note: WebViewがメインフレームのコンテンツの受信を開始したときに呼ばれます。webView(_:decidePolicyFor:decisionHandler:)がナビゲーションのレスポンスを承認した後、WebViewは処理を開始、変更の準備が整うと、WebViewはメインフレームを更新し始める前に、このメソッドを呼び出し
     func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
-        print("didCommit navigation: WKNavigation!")
+        print("ページ読み込み開始")
     }
     /// リクエストのロード進行状況の追跡(タイミング：リダイレクト時)
     ///  - note: WebViewが要求のサーバーリダイレクトを受信したときに呼び出し🔍
@@ -84,18 +86,18 @@ extension WebViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView,
                  didReceive challenge: URLAuthenticationChallenge,
                  completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-        print("didReceive challenge: URLAuthenticationChallenge")
+        print("認証チャレンジ")
         completionHandler(.useCredential, nil)
     }
     /// エラー対応😭
     /// - note: ページ読み込み途中でのエラー発生時（HTML読み込み中のキャンセルなど）に呼び出し
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: any Error) {
-        print("エラー: \(error.localizedDescription)")
+        print("ページ読み込み途中でのエラー")
     }
     /// エラー対応😭
     /// - note: ページ読み込み開始時でのエラー発生時（通信圏外など）に呼び出し
     func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError: Error) {
-        print("didFailProvisionalNavigation navigation: WKNavigation!")
+        print("ページ読み込み開始でのエラー")
     }
     
 }
